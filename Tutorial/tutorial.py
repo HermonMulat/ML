a5c7297859124cf3e14ebf63fa29950d7c46ba45
Hermon <hermonmulat@gmail.com>
2018-01-22 15:45:05 -0500

    Add a score for model

--- Tutorial/tutorial.py
+++ Tutorial/tutorial.py
@@ -1,28 +1,35 @@
 import random
 import matplotlib.pyplot as plt
 from sklearn import linear_model
+import numpy as np
 
 def f(x,m=2.71828, error = 0.1):
-    # return a x,y and y = m*x with some error pecentage - default is with 25%
+    # return a x,y and y = m*x with some error pecentage - default is with 10%
     y = m*x
     e = y*random.uniform(-1*error, error)
     return (x,y+e)
 
 def main():
-    values = [f(random.uniform(1,300)) for i in xrange(100)]
+    values = [f(random.uniform(1,1000)) for i in xrange(100)]
     train_set = values[:-20]
     test_set = values[-20:]
 
     # create LinearRegression model
-    model = linear_model.LinearRegression(fit_intercept=False)
+    model = linear_model.LinearRegression(fit_intercept=True)
     x,y = zip(*train_set)
     x = map(lambda a:[a],x)
     model.fit(x,y)
     m,b = model.coef_[0], model.intercept_
-    print "Best fit line: y = {0:.4f}x + {1:.4f}".format(m,b)
+    print "Best fit line: y = {0:.5f}x + {1:.5f}".format(m,b)
     test_x,test_y =  zip(*test_set)
     test_x = map(lambda a:[a], test_x)
     predicted = model.predict(test_x)
+
+    # measure the accuracy of prediction by model
+    mean_sq_err = np.mean((np.array(predicted)-np.array(test_y))**2)
+    print "Mean squared error: {0:.5f}".format(mean_sq_err)
+    # variance score
+    print "Variance score:  {0:.5f}".format(model.score(test_x,test_y))
 
     # plot results
     x,y = zip(*train_set)
